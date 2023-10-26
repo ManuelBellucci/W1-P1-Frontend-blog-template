edd0f00f1c9d40a944c7fefeed1611ca272ab7f2
Manuel Bellucci <matiasbelluccimanuel@gmail.com>
2023-10-26 12:55:21 +0200

    fixed handleDelete bug and .render to createRoot

--- src/components/comments/Comment.jsx
+++ src/components/comments/Comment.jsx
@@ -1,22 +1,9 @@
 // imports
 import React from 'react'
-import axios from 'axios'
 import { Button } from 'react-bootstrap'
 
 // Componente Comment che mostra un singolo commento
 const Comment = ({ id, postId, author, content, onDeleteComment }) => {
-  const handleDeleteComment = async () => {
-    try {
-      await axios.delete(
-        `http://localhost:3001/api/posts/${postId}/comments/${id}`
-      )
-      // Se la richiesta di eliminazione ha successo, chiamare la funzione di callback per aggiornare lo stato nel componente padre
-      onDeleteComment(id)
-      console.log('Commento eliminato con successo!')
-    } catch (error) {
-      console.error("Errore durante l'eliminazione del commento:", error)
-    }
-  }
   return (
     <div>
       <strong>User: </strong>
@@ -25,7 +12,7 @@ const Comment = ({ id, postId, author, content, onDeleteComment }) => {
         <strong>Comment: </strong>
         {content}
       </p>
-      <Button variant='danger' onClick={handleDeleteComment}>
+      <Button variant='danger' onClick={() => onDeleteComment(id)}>
         Elimina commento
       </Button>
     </div>
